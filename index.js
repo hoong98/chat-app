2484befb0742a1f077c3b8d7e844d1299bbbdb78
hoong98 <wwhoong98@gmail.com>
2020-11-03 11:28:56 +0800

    deploy 2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 // app.use(router)
 // Serve any static files
 app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
+// Handle React routing, return all requests to React app;
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
